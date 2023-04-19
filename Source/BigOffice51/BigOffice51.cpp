1cbce558f2caa698d40c4068879821082ceb247c
laffet12 <42987534+laffet12@users.noreply.github.com>
2023-04-19 19:37:16 +0900

    Revert "test1"
    This reverts commit 8dbd1921aca7101675f65aa213489c98f7d544fa.

--- Source/BigOffice51/BigOffice51.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BigOffice51.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BigOffice51, "BigOffice51" );
